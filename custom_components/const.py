dca9d52bff78f1e1b59bfe78a3a318dd5818470d
confucius78 <dekker.wj@gmail.com>
2022-11-14 21:58:15 +0100

    Revert "Add files via upload"
    This reverts commit 1ce34923e1606657ed6a17d02eb9163c654b9546.

--- custom_components/const.py
+++
@@ -1,7 +0,0 @@
-"""Constants for the Detailed Hello World Push integration."""
-
-# This is the internal name of the integration, it should also match the directory
-# name for the integration.
-DOMAIN = "climax"
-
-CLIMATE_DOMAIN = "climate"
